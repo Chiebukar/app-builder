cbd95bc58d6bef752699234e58721c74e9121645
Chiebukar <sam23chuks@gmail.com>
2026-02-01 18:31:28 +0100

    added architect prompt to define implementation tasks

--- agent/prompts.py
+++ agent/prompts.py
@@ -4,3 +4,21 @@ def planner_prompt(user_prompt: str) -> str:
                         User request: {user_prompt}
                     """
     return planner_prompt
+
+def architect_prompt(plan: str) -> str:
+    architect_prompt = f"""
+                        You are the architect agent. Based on the following project plan, break it down into explicit engineering tasks.    
+
+                        Rules:
+                        - For each file in the plan, create one or implementation tasks.
+                        - in each task description, specify:
+                            * The task to implement.
+                            * The name of the variables, functions, classes, annd componets to be defined.
+                            * Mention how the task depends on or will be used by the previous and next tasks.
+                            * Include integration details: imports, expected functions signatures, data flow.
+                        - Order tasks so that dependnencies are implemented first.
+                        - Each step must be self -contained but also carry forward the context from previous steps.
+
+                        Project Plan: {plan}
+                    """
+    return architect_prompt
